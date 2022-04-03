e358e827a22347400bf63de1bcd58edc33d0b708
Autentico203 <90540647+Autentico203@users.noreply.github.com>
2022-04-03 22:24:59 +0800

    Add files via upload

--- computing1/local_queue.cpp
+++ computing1/local_queue.cpp
@@ -6,7 +6,7 @@
 #include <cstring>
 #include <stdlib.h>
 #include <time.h>
-
+#include <vector>
 using namespace std;
 
 template<class T> fifo<T>::fifo(int size)
@@ -121,30 +121,204 @@ template<class T>void fifo<T>::deallocate()
 
 
 
-template<class T>void person<T>::random_generate()
-{
-
-  
-}
 
 
 template<class T> person<T>::person()
 {
-    srand((unsigned)time(0));
+    srand((unsigned)time(NULL));
     string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int i;
-    //Create name
+    string integer_set = "0123456789";
+    int i;//index for loop
+    int index;//index for alphabet,interger_set and other vectors;
+    //Create random name
     int namelength = rand() % 5 + 2;
     name.resize(namelength);
     for(i = 0; i < namelength;i++)
     {
-        int index = rand() % 26;
+        index = rand() % 26;
         name[i] = alphabet[index];
     }
-    cout << name << endl;
+
+    //Create id
+    int idlength = 10;
+    id.resize(idlength);
+    for(i = 0; i < idlength; i++)
+    {
+        index = rand() % 10;
+        id[i] = integer_set[index];
+    }
+
+    //Create address
+    vector<string>address_set;
+    address_set.push_back("Xiashi"); address_set.push_back("Haizhou"); address_set.push_back("Haichang"); address_set.push_back("Maqiao");
+    int address_set_id = rand() % address_set.capacity();
+    address = address_set[address_set_id];
+
+    //Create phone
+    int phonelength = 11;
+    phone.resize(phonelength);
+    for(i = 0; i < phonelength; i++)
+    {
+        index = rand() % 10;
+        phone[i] = integer_set[index];
+    }
+
+    //Create Wechat
+    int Wechatlength = 19;
+    Wechat = "wxid_";
+    Wechat.resize(Wechatlength);
+    for(i = 0; i < Wechatlength - 5;i++)
+    {
+        index = rand() % 26;
+        Wechat[i+5] = alphabet[index];
+    }
+    //Change three letters to digits
+    for(i = 0;i < 3;i++)
+    {
+        index = rand() % 13 + 5;
+        int int_index = rand() % 10;
+        Wechat[index] = integer_set[int_index];
+    }
+
+    //Create email
+    vector<string>email_suffix_set;
+    email_suffix_set.push_back("@qq.com"); email_suffix_set.push_back("@gmail.com"); email_suffix_set.push_back("@163.com");  email_suffix_set.push_back("@sina.com");
+    int emaillength = 10;
+    string email_prefix;
+    email_prefix.resize(emaillength);
+    for(i = 0; i < emaillength; i++)
+    {
+        index = rand() % 10;
+        email_prefix[i] = integer_set[index];
+    }
+    index = rand() % 4;
+    email = email_prefix + email_suffix_set[index];
     
+    //Create prof
+    vector<string>prof_set;
+    //Profession priority increase from this order
+    prof_set.push_back("Teacher"); prof_set.push_back("Actor"); prof_set.push_back("Farmer"); prof_set.push_back("Businessman");
+    prof_set.push_back("Journalist"); prof_set.push_back("Student"); prof_set.push_back("Engineer"); prof_set.push_back("Programmer");
+    index = rand() % 8;
+    prof = prof_set[index];
+    prof_id = index;
+
+    //Create age;
+    vector<string>age_set;
+    age_set.push_back("Children"); age_set.push_back("Adolescent"); age_set.push_back("Young Adult"); age_set.push_back("Adult");
+    age_set.push_back("Senior"); age_set.push_back("Elderly Person"); age_set.push_back("Old Person");
+    index = rand() % 7;
+    age = age_set[index];
+    age_id = index;
+
+    //Create risk
+    vector<string>risk_set;
+    risk_set.push_back("No risk"); risk_set.push_back("Low risk"); risk_set.push_back("Medium risk"); risk_set.push_back("High risk");
+    index = rand() % 4;
+    risk = risk_set[index];
+    risk_id = index;
+
+    //Create Time
+    Time = new double[3];
+    Time[0] = rand() % 2015 + 0.1 * (rand() % 10);//2016时暂不考虑  
+    Time[1] = -1;
+    Time[2] = -1;   //-1表示不知道目前appointment time和treatment time为什么时候
+
+
+    //Create Withdraw and priority letter;
+    withdraw = false;
+    letter = false;
+
 
 }
+
+
+template<class T> double* person<T>::show_hour()
+{
+    return Time;
+}
+
+template<class T> int* person<T>::show_day()
+{
+    int* Day = new int[3];
+    for(int i = 0;i < 3; i++)
+    {
+        if(Time[i] == -1){
+            Day[i] = -1;
+            continue;
+        }   
+        Day[i] = (int)(Time[i] / 24);
+    }
+    return Day;
+}
+
+template<class T> int* person<T>::show_week()
+{
+    int* Week = new int[3];
+    for(int i = 0; i < 3; i++)
+    {
+        if(Time[i] == -1){
+            Week[i] = -1;
+            continue;
+        }
+        Week[i] = (int)(Time[i] / (24 * 7));   
+    }
+    return Week;
+}
+
+template<class T> int* person<T>::show_month()
+{
+    int* Month = new int[3];
+    for(int i = 0; i < 3;i++)
+    {
+        if(Time[i] == -1){
+            Month[i] = -1;
+            continue;;
+        }
+        Month[i] = (int)(Time[i] / (24*28));    
+    }
+    return Month;
+}
+
+
+template<class T>string* person<T>::show_format_time()
+{
+    int* Month = new int[3];
+    int* Day = new int[3];
+    double* Hour = new double[3];
+    int* Minute = new int[3];
+    string* format_time = new string[3];
+
+    //Set month,day,hour,minute
+    Month = this->show_month();
+    Day = this->show_day();
+    Hour = this->show_hour();
+    for(int i = 0; i < 3;i++)
+    {
+        if(Time[i == -1])
+        {
+            Minute[i] = -1;
+            continue;
+        }
+        int intHour = (int)Hour[i];
+        Minute[i] = (int) ((intHour - Hour[i]) * 60);
+    }
+    for(int i = 0; i < 3;i++)
+    {
+        if(Time[i == -1])
+        {
+            format_time[i] = "N/A";
+            continue;
+        }
+        format_time[i] = to_string(2022) + "-" + to_string(1+Month[i]) + "-" + to_string(Day[i] % 28 + 1) + " " + to_string(intHour[i] % 24 ) + "-" + to_string(Minute[i]);
+        //Format: 2022-3-27 19:22
+        return format_time;
+    }
+
+
+
+}
+
 template<class T> void person<T>::set_key()
 {
     //525699 is the largest time in minute within a year
@@ -152,17 +326,17 @@ template<class T> void person<T>::set_key()
     key = 525699 - calculate_time;
 
     //Withdraw priority
-    if ((withdraw == true) && ((risk == 0) || (risk == 1)))
+    if ((withdraw == true) && ((risk_id == 0) || (risk_id == 1)))
     {
         key += 14 * 24 * 60;
     }
 
     //risk priority
-    if( risk == 2)
+    if( risk_id == 2)
     {
         key += 30 * 24 * 60;    //Medium risk extend one month
     }
-    if( risk == 3)
+    if( risk_id == 3)
     {
         //if( local_queue_id.is != true)
         {
@@ -172,15 +346,14 @@ template<class T> void person<T>::set_key()
     }
 
     //age priority
-    key += age * 1000000;
+    key += age_id * 1000000;
     //profession priority
-    key += prof * 10000000;
+    key += prof_id * 10000000;
     //letter priority
     if (letter == true)
     {
         key += 100000000;
     }
-    
     return;
 }
 
@@ -192,6 +365,15 @@ template<class T> T person<T>::return_key()
 int main()
 {
     person<int> p1;
-    p1.random_generate();
+    double* t1;
+    int* t2;
+    t1 = p1.show_hour();
+    t2 = p1.show_day();
+    
+    cout << t1[0] << endl;
+    cout << t2[0] << endl;
+    cout << p1.show_week()[0] << endl;
+    cout << p1.show_month()[0] << endl;
+    cout << p1.show_format_time() << endl;
 
 }
--- computing1/local_queue.h
+++ computing1/local_queue.h
@@ -36,21 +36,34 @@ public:
     T return_key();
     void update_status(int status_number);
     string show_name();
-    string show_prof();
-    string show_age();
-    string show_risk();
-    string show_waitingtime();
+    int show_prof();
+    int show_age();
+    int show_risk();
 
+    double* show_hour();
+    int* show_day();
+    int* show_week();
+    int* show_month();  //从零开始的统一时间
+    string* show_format_time();
 
+    bool is_withdraw();
+    bool is_appointed();
+    void update_appointed();
     int status;
 private:
     bool initial_register;  //true if register for the first time
     bool letter;    //true if there is letter for ddl
-    int risk;   //0,1,2,3
-    int prof;   //0,1,2,3,4,5,6,7, the larger, the higher priority
-    int age;    //0,1,2,3,4,5,6, the larger, the higher priority
+    int risk_id;
+    string risk;   //0,1,2,3
+    int prof_id;   //0,1,2,3,4,5,6,7, the larger, the higher priority
+    string prof;    //String name for profession
+    int age_id;
+    string age;    //0,1,2,3,4,5,6, the larger, the higher priority
     bool withdraw;  //true if withdraw
+    bool appointed; //True if apppointment is made
+    
     fifo<person<T>*> local_registry_id;    //Which Local registry this guy is in
+    double* Time; //表示时间的数组，0为registeration，1为appointment， 2为treatment
     //appointment_list
     //hospital
     //appointment_time出堆时间
@@ -67,7 +80,6 @@ private:
     T key;
 
 };
-
 
 
 
