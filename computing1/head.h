fcfe920d5b5b389a64ea92b43d2702885df34d51
Autentico203 <90540647+Autentico203@users.noreply.github.com>
2022-04-05 22:52:26 +0800

    fibheap class updated
    Using new class from others(by reference)

--- computing1/head.h
+++ computing1/head.h
@@ -22,6 +22,8 @@ public:
     T front(void);
     void pushback(T value);
     T popfront();
+
+
 private:
     int maxsize, minsize;
     int first, last;
@@ -29,42 +31,54 @@ private:
     T* reprarray;
     void allocate(void);
     void deallocate(void);
+    
+
 };
 
  
 template <class T>class person
 {
 public:
-    friend class TableWrite;
-    friend class Weeklyreport;
-    friend class Monthlyreport;
-    person();       //If no argument, create one,key is set as well
-    person(string tableline);   //Create a person from local registry
+    person();       //If no argument, create one
+    person(string tableline);   //Create a person from local registry,需要计算keyvalue
     void random_generate(int seed); //Used to put random attributes into a person
-    void set_key(); 
+    void set_key();
     T return_key();
     void update_status(int status_number);
-    string show_id();
+    // string show_name();
+    // int show_prof();
+    // int show_age();
+    // int show_risk(); //对重要的类来说这些都可以直接友元访问，若其他情况下需要再添加所需函数即可
+    
     double* show_hour();
-    int* show_intHour();
     int* show_day();
-    int* show_half_day();
     int* show_week();
     int* show_month();  //从零开始的统一时间
     string* show_format_time();
-    bool is_update(person<int>* oldper); //check whether this is an update
-    bool is_newwithdraw(person<int>* oldper);//check whether this is a new withdraw
-    bool is_withdraw(); //return withdraw
-    void set_appointment(appointment* r_appoint);    //Not written yet
-    void punish();   //Not written yet
-    void display_all(); //Display all information of this person
-
-    int local_id;   
-    int* hospital_id;    //Array with index for the hospital，即hopsital's ranking list 
-    int hosp_num;   //Initailized when the person is created
+    
+    bool is_update(person<int>* oldper); //check whether this is an update需要写
+    bool is_newwithdraw(person<int>* oldper);//check whether this is a new withdraw需要写
+    //可以大概这样写
+    /* template<class T> bool person::is_update(person<int>* oldper){
+      if (withdraw == oldper->is_withdraw()){return true;}
+      else{return false;}
+      }
+    teplate<class T> bool person::is_newwithdraw(person<int>* oldper){
+      if (withdraw == true && oldper->is_withdraw()){return true;}
+      else{return false;}
+    }*/
+    bool is_withdraw();//是不是还没写，是return withdraw 吗
+    bool is_appointed();//可能不需要写，appointment 是NULL 就代表未appointment,如果要保留在set_appointment 里set此bool
+    void update_appointed();//可能重复了(就是set_appointment吧，reportlist 写了 set_appointment
+    int local_id;
     int status;
-    appointment* p_appoint;
+    int hosp_num;//初始化的时候别忘，否则make appointment 会无限死循环
+    vector<int> hospital_ranking_list;//the hospital's  ranking list with hospital id as element
+    void set_appointment(appointment* r_appoint);//需要写
+    void punish();//需要写
+
 private:
+    bool initial_register;  //true if register for the first time//是否就是status,不需要了
     bool letter;    //true if there is letter for ddl
     int risk_id;
     string risk;   //0,1,2,3
@@ -72,31 +86,21 @@ private:
     string prof;    //String name for profession
     int age_id;
     string age;    //0,1,2,3,4,5,6, the larger, the higher priority
+    int local_registry_id;  //Which Local registry this guy is in
+    bool withdraw;  //true if withdraw
+    bool appointed; //True if apppointment is made//可能不需要写，appointment 是NULL 就代表未appointment
+    
+    double* Time; //表示时间的数组，0为registeration，1为appointment， 2为treatment
+                  //0在初始化的时候写入，1，2在setappointment 函数里写入？还是得由主函数写入
     string id;
     string name;
     string address;
     string phone;
     string Wechat;
     string email;
-    bool withdraw;  //true if withdraw
-    double* Time; //表示时间的数组，0为registeration，1为appointment,
-                //0在初始化设置，1在创建时初始化为-1,具体的1时间应该由set_appoitment写入
     T key;
-
+    appointment* p_appoint;//需要初始化为NULL
 };
-
-class TableWrite
-{
-public:
-    void table_create(string filename,int num); //num:创建的人的个数,
-    string table_line(person<int>& p); //给每一个人创造一行
-
-private:
-    ofstream outfile;
-    void table_open(string filename);
-    void table_close();
-};
-
 
 template<class T> class Fb_heap
 {
@@ -115,7 +119,7 @@ private :
     };
 private :
     void listadd(Fb_node * &r, Fb_node * ptr);// add node ptr to the right of node r
-    //int Dn() {return (log2(numnodes) + 1);} 
+    int Dn() {return (log2(numnodes) + 1);} 
     void deleteln(Fb_node *p) ; //delete the node in list
     void linkheap(Fb_node * p1, Fb_node * p2); //link p1 to the right of p2
     void cutheap(Fb_node * p1, Fb_node * p2) ;
@@ -138,18 +142,18 @@ public:
 class appointment
 {
 public:
-  appointment(person<int>* a_patient, int date_out_of_queue);
+  appointment(person<int>* a_patient, int date_out_of_queue,vector<hospital*> a_hospital_list);
   //virtual ~appointment();
   void make_appointment();
-  void appointment_withdraw();
   void pretty_print();
-  bool is_appointment_passed();
+  bool is_appointment_passed(double current_hour);
   int hospital_id;
   person<int>* patient;
-  double the_time;
-  int date;
+  vector<hospital*> hospitals;
     
-
+private:
+  int date;
+  double time;
 };
 
 class hospital
@@ -158,10 +162,12 @@ public:
   hospital(int idnum, int capacity, double o_time, double c_time);
   bool is_hospital_available(int h_date);
   double get_time_appointed(int h_date);
+  int daily_appointment_num;
   int id;//the hospital's unique id number, which marks its position in the hospital list.
   vector<appointment*> daily_appointment_list;
   void add_to_applist(appointment* new_app);
-  void printapp();
+  void withdraw_app(appointment* w_app);//需要写，将appointment 从vector里面删除， 并且将那个人的appointment 重新设为 NULL
+  void printapp(int h_date);
   int daily_remaining_capacity[100];
 private:
   int daily_capacity;
@@ -243,6 +249,8 @@ private:
 
     vector<person<int>*> rl;
 };
+
+
 
 
 #endif
